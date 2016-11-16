5bd7122dc5a16053c6d85e76092bc1618efd132d
Paige <paige.vogenthaler@gmail.com>
2016-11-16 10:29:45 -0800

    Complete

--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,12 @@
 class Grub {
+  constructor(){
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';s
+  }
+  eat () {
+    return 'Mmmmmmmmm jelly';
+  }
 };
 
 export default Grub;
